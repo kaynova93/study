4ab6f8048a27f575c8e5dccec24a4ded4014d76c
kaynova93 <kaynova.93@mail.ru>
2023-11-06 12:25:27 +0300

    Задача 3. Массивы

--- src/com/ibs/Main.java
+++ src/com/ibs/Main.java
@@ -2,7 +2,13 @@ package com.ibs;
 
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Hello World!");
 
-        System.out.print("Hello World!");
+        //Задача 3
+        TaskArrays tdt = new TaskArrays();
+        tdt.changeElements(0,4);
+
+        tdt.resultSum();
     }
 }
